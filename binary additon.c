c78d08c59590b2af81b44ceb512c9ea1687b8755
Aadityaza <45432533+Aadityaza@users.noreply.github.com>
2019-08-02 09:30:57 +0300

    Update binary additon.c

--- binary additon.c
+++ binary additon.c
@@ -2,19 +2,24 @@
 void add (int x,int y,int z){
     int c=(x&&y);
     int s=(x^y)^z;
-    printf("%d\n%d",s,c);
-    
+    printf("%d\n",s);
+
 }
 int main ()
 {
     int a=1010,b=0001;
-    for(int i=0;a==0&&b==0;i++){
+    while(1){
       int x=a%10;
+      
       int y=b%10;
       int z=0;
       a/=10;
       b/=10;
+      
     add(x,y,z);
+    if(a==0&&b==0){
+          break;
+      }
     }
     
 }
